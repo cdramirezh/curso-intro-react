f92b8544911a050e7d49ac9c387d153f4dafbdc2
higi <cdramirezh@unal.edu.co>
2023-02-14 16:07:32 -0500

    hig order components

--- src/index.js
+++ src/index.js
@@ -3,9 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// function App(props) {
+//   return (
+//     <h1>{props.saludo}, {props.nombre}</h1>
+//   )
+// }
+
+// function withSaludo(WrappedComponent) {
+//   return function WrappedComponentWithSaludo(saludo) {
+//     return function ComponenteDeVerdad(props) {
+//       return <>
+//         <WrappedComponent {...props} saludo={saludo} />
+//         <p>Accompany</p>
+//       </>
+//     }
+//   }
+// }
+
+// const AppWithSaludo = withSaludo(App)('Wenas');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <App saludo="Buenas" nombre="Nath" /> */}
+    {/* <AppWithSaludo saludo="Hey" nombre="Juanita" /> */}
     <App />
   </React.StrictMode>
 );
